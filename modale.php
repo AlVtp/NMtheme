b25c302004b4c572b634c04ecc9c969665f97615
admin <alexvitip@gmail.com>
2024-02-07 17:59:52 +0100

    progrés modale

--- modale.php
+++ modale.php
@@ -4,7 +4,9 @@
   <!-- Modal content -->
   <div class="modal-content">
     <span class="close">&times;</span>
-    <p>Some text in the Modal..</p>
+    <img class="modalHeader" src="/wp-content/themes/NMtheme/assets/images/contactHeader.jpg" alt='Image header de la modale contact'>
+    <div class="modalForm">
+      <?php echo do_shortcode('[contact-form-7 id="a6757cf" title="modalForm"]'); ?>
+    </div>
   </div>
-
 </div>
